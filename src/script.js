2159664abfc8afbd9c779b8108995c9af044fe8a
songihyeon <tyumr70@gmail.com>
2025-02-21 16:10:43 +0900

    task completed

--- src/script.js
+++ src/script.js
@@ -1,9 +1,39 @@
-// TODO : DOM으로부터 필요한 엘리먼트를 불러오세요.
+document.addEventListener("DOMContentLoaded", function () {
+  let elInputUsername = document.querySelector("#username");
+  let elFailureMessage = document.querySelector(".failure-message");
+  let elSuccessMessage = document.querySelector(".success-message");
 
+  let paInputPassword = document.querySelector("#password");
+  let paFaliureMessage = document.querySelector(".mismatch-message");
+  let paFaliureRetype = document.querySelector("#password-retype");
+
+  // 아이디 입력 이벤트
+  elInputUsername.onkeyup = function () {
+    if (isMoreThan4Length(elInputUsername.value)) {
+      elSuccessMessage.classList.remove("hide");
+      elFailureMessage.classList.add("hide");
+    } else {
+      elSuccessMessage.classList.add("hide");
+      elFailureMessage.classList.remove("hide");
+    }
+  };
+
+  // 비밀번호 확인 입력 이벤트
+  paFaliureRetype.onkeyup = function () {
+    if (isMatch(paInputPassword.value, paFaliureRetype.value)) {
+      paFaliureMessage.classList.add("hide"); // 비밀번호 일치하면 숨김
+    } else {
+      paFaliureMessage.classList.remove("hide"); // 불일치하면 표시
+    }
+  };
+});
+
+// 아이디 길이 검사 함수
 function isMoreThan4Length(value) {
-  // TODO : 이 함수를 완성하세요.
+  return value.length >= 4;
 }
 
+// 비밀번호 일치 검사 함수
 function isMatch(password1, password2) {
-  // TODO : 이 함수를 완성하세요.
+  return password1 === password2;
 }
